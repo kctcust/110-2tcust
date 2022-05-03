c4af1c3d3d9938e74af76a17fc1f97f39c1837b3
kctcust <100654115+kctcust@users.noreply.github.com>
2022-05-03 09:57:26 +0800

    Add files via upload

---
+++ HTML5/week12/week12-2.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "usbw";
+$dbname = "myDB1";
+$tbname = "MyTable1";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM $tbname";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["myname"]. " - email:" . $row["myemail"]."<br>";
+  }
+} else {
+  echo "0 results";
+}
+
+$conn->close();
+?>
+</body>
+
+</html>
