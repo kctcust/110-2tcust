84be83d14db5429ad9fef6034cbfeafd5460014e
kctcust <100654115+kctcust@users.noreply.github.com>
2022-05-10 10:24:50 +0800

    Add files via upload

---
+++ HTML5/week13/createRecordb.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "usbw";
+    $dbname = "myDB1";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    mysqli_query($conn, "set names utf8");
+    for ($i = 1; $i <= 20; $i++) {
+        $sql = "INSERT INTO MyTable1 (myname, myemail, myaddress) VALUES ('小華$i', 'john$i@example.com', '花蓮$i')";
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully<br>";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    }
+    $conn->close();
+    ?>
+</body>
+
+</html>
