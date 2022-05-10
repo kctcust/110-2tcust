b6d2b8402d5d0bdf06f00f2aa0c884eb85b5e9c2
kctcust <100654115+kctcust@users.noreply.github.com>
2022-05-10 10:01:01 +0800

    Add files via upload

---
+++ HTML5/week13/createTablea.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "usbw";
+    $dbname = "myDB1";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // sql to create table
+    $sql = "CREATE TABLE MyTable1 (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+myname VARCHAR(30) NOT NULL,
+myemail VARCHAR(30) NOT NULL,
+myaddress VARCHAR(50),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) character set UTF8";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Table created successfully";
+    } else {
+        echo "Error creating table: " . $conn->error;
+    }
+
+    $conn->close();
+
+    ?>
+</body>
+
+</html>
