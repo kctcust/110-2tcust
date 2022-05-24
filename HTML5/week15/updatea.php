0ce6455e9c471a9144874c46086b3dc0c3fb17a9
kctcust <100654115+kctcust@users.noreply.github.com>
2022-05-24 09:29:07 +0800

    Add files via upload

---
+++ HTML5/week15/updatea.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.css" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Document</title>
+</head>
+
+<body>
+<?php include "nav1.html" ?>
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "usbw";
+    $dbname = "myDB1";
+    $tbname = "myTable1";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    mysqli_query($conn, "set names utf8");
+
+    // $sql = "INSERT INTO $tbname (myname, myemail, myaddress) VALUES ('$myname', '$myemail', '$myaddress')";
+    $sql = "UPDATE $tbname SET myname='Doe' WHERE id=2";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "record updated successfully<br>";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    $conn->close();
+    ?>
+</body>
+
+</html>
