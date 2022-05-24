da34b29eadeb38a5eeb4e6d8559e9260b9180879
kctcust <100654115+kctcust@users.noreply.github.com>
2022-05-24 10:29:40 +0800

    Add files via upload

---
+++ HTML5/week15/UI+delete.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.css" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Document</title>
+    <style>
+        .container {
+            width: 100%;
+            max-width: 700px;
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <?php include "nav1.html" ?>
+    <div class="container">
+        <form action="update.php" method="GET">
+            <p>id: <input type="text" name="id"></p>
+            <input type="submit" value="刪除資料" class="btn btn-primary">
+            <button type="reset" value="Reset" class="btn btn-info">Reset</button>
+        </form>
+    </div>
+</body>
+</html>
