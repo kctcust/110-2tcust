5fee4f1acb434465850b6a698d7589150d5691eb
kctcust <100654115+kctcust@users.noreply.github.com>
2022-05-31 10:54:37 +0800

    Add files via upload

---
+++ HTML5/week16/uploada.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.css" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Document</title>
+    <style>
+        .container {
+            width: 100%;
+            max-width: 700px;
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <?php include "nav1.html" ?>
+    <div class="container">
+        <?php
+        $target_dir = "uploads/";
+        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+        // $uploadOk = 1;
+        // $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+        // Check if image file is a actual image or fake image
+        // if (isset($_POST["submit"])) {
+        //     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        //     if ($check !== false) {
+        //         echo "File is an image - " . $check["mime"] . ".";
+        //         $uploadOk = 1;
+        //     } else {
+        //         echo "File is not an image.";
+        //         $uploadOk = 0;
+        //     }
+        // }
+        
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+          } else {
+            echo "Sorry, there was an error uploading your file.";
+          }
+
+        ?>
+    </div>
+</body>
+
+</html>
