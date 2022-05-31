1bbfdd107416fb7531c7338b25483ca5ed5742ab
kctcust <100654115+kctcust@users.noreply.github.com>
2022-05-31 09:24:01 +0800

    Add files via upload

---
+++ HTML5/week16/UI+wherec.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.css" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Document</title>
+    <style>
+        .container {
+            width: 100%;
+            max-width: 700px;
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <?php include "nav1.html" ?>
+    <div class="container">
+        <form action="wherec.php" method="GET">
+            <p>最小id: <input type="text" name="id1"></p>
+            <p>最大id: <input type="text" name="id2"></p>
+            <p>"OR" 名字Name: <input type="text" name="name"></p>
+            <input type="submit" value="篩選資料" class="btn btn-primary">
+            <button type="reset" value="Reset" class="btn btn-info">Reset</button>
+        </form>
+    </div>
+</body>
+</html>
